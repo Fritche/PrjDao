18de2c38b2d2bfa1351d83e262d37137c533475d
Fritche <fritche@gmail.com>
2020-08-31 16:10:43 -0300

    Seller

--- src/apps/TstClass.java
+++ src/apps/TstClass.java
@@ -1,12 +1,18 @@
 package apps;
 
+import java.util.Date;
+
 import model.entidades.Department;
+import model.entidades.Seller;
 
 public class TstClass {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
+		Seller seller = new Seller(21, "Ze", "email@email.com", new Date(), 3000.00, obj);
 		System.out.println(obj);
+		System.out.println(seller);
+		
 
 	}
 
