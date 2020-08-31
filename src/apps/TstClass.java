a9c73d3c65d6cced438fb9f5183ad049fd9f1224
Fritche <fritche@gmail.com>
2020-08-31 15:59:01 -0300

    Depto class

---
+++ src/apps/TstClass.java
@@ -0,0 +1,13 @@
+package apps;
+
+import model.entidades.Department;
+
+public class TstClass {
+
+	public static void main(String[] args) {
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
